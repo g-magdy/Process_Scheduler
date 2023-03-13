6b15a15892a3a3d2709e3fb5419a3e775ea8dff1
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-03-13 21:44:01 +0200

    Add project files.

---
+++ Basic/Node.h
@@ -0,0 +1,49 @@
+#pragma once
+
+template <class ItemType>
+class Node
+{
+	ItemType data;
+	Node<ItemType>* next;
+public:
+	Node();
+	Node(const ItemType& entry);
+	void setData(const ItemType& entry);
+	void setNext(Node<ItemType>* ptr);
+	ItemType getData() const;
+	Node<ItemType>* getNext() const;
+};
+
+template<class ItemType>
+inline Node<ItemType>::Node() : next(nullptr)
+{
+}
+
+template<class ItemType>
+inline Node<ItemType>::Node(const ItemType& entry) : data(entry)
+{
+}
+
+template<class ItemType>
+inline void Node<ItemType>::setData(const ItemType& entry)
+{
+	data = entry;
+}
+
+template<class ItemType>
+inline void Node<ItemType>::setNext(Node<ItemType>* ptr)
+{
+	next = ptr;
+}
+
+template<class ItemType>
+inline ItemType Node<ItemType>::getData() const
+{
+	return data;
+}
+
+template<class ItemType>
+inline Node<ItemType>* Node<ItemType>::getNext() const
+{
+	return nullptr;
+}
