fef802b30f32439f0387b4d2e7a25843f51d09e5
Ahmed Aladdin <racerahmed40@gmail.com>
2023-03-14 16:13:04 +0200

    add queue class to main to test it

--- test130323.cpp
+++ test130323.cpp
@@ -1,5 +1,6 @@
 #include "Basic/LinkedList.h" // Note that I ned to specify the path
 // iostream is already included in LinkedList.h 
+#include "Basic/Queue.h"
 
 int main()
 {
@@ -9,5 +10,12 @@ int main()
 		mylist.insertEnd(6 * i);
 	}
 	mylist.printlist();
+
+	Queue<int> myqueue;
+	for (int i = 0; i < 5; i++)
+	{
+		myqueue.push(7 * i);
+	}
+	myqueue.print();
 	return 0;
 }
