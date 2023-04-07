6fb2c5d53136a0a802c1ae39f8762c6663bd9276
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-07 14:10:28 +0200

    Created the Process Class

---
+++ Components/Process.cpp
@@ -0,0 +1,128 @@
+#include "Process.h"
+
+Process::Process(std::string id, int inAT, int ct) : AT(inAT), ID(id), CT(ct)
+{
+	RT = TT = TRT = WT = -1; // they are not givn real values yet
+	FCT = 0;
+	currentState = NEW;
+	handlingCPU = NoCPU;
+	myChild = nullptr;
+}
+
+Process::Process(const Process& origin)
+{
+	ID = origin.ID;
+	currentState = origin.currentState;
+	handlingCPU = origin.handlingCPU;
+	AT = origin.AT;
+	RT = origin.RT;
+	CT = origin.CT;
+	FCT = origin.FCT;
+	TT = origin.TT;
+	TRT = origin.TRT;
+	WT = origin.WT;
+	IOList = origin.IOList;
+	myChild = nullptr;      //what should I set this to ?
+}
+
+std::string Process::getID() const
+{
+	return ID;
+}
+
+void Process::setProcessState(ProcessState CS)
+{
+	currentState = CS;
+}
+
+ProcessState Process::getProcessState() const
+{
+	return currentState;
+}
+
+void Process::setHandlingCPU(CPU_TYPE HCPU)
+{
+	handlingCPU = HCPU;
+}
+
+CPU_TYPE Process::getHandlingCPU() const
+{
+	return handlingCPU;
+}
+
+int Process::getAT() const
+{
+	return AT;
+}
+
+void Process::setRT(int rt)
+{
+	RT = rt;
+}
+
+int Process::getRT() const
+{
+	return RT;
+}
+
+int Process::getCT() const
+{
+	return CT;
+}
+
+void Process::updateFCT(int n = 1)
+{
+	FCT += n;
+}
+
+int Process::getFCT() const
+{
+	return FCT;
+}
+
+void Process::setTT(int tt)
+{
+	TT = tt;
+	TRT = TT - AT;
+	WT = TRT - CT;
+}
+
+int Process::getTT() const
+{
+	return TT;
+}
+
+int Process::getTRT() const
+{
+	return TRT;
+}
+
+int Process::getWT() const
+{
+	return WT;
+}
+
+Process* Process::getMyChild()
+{
+	return myChild;
+}
+
+bool Process::operator>(const Process& second)
+{
+	return (CT > second.CT);
+}
+
+bool Process::operator<(const Process& second)
+{
+	return CT < second.CT;
+}
+
+bool Process::operator>=(const Process& second)
+{
+	return CT >= second.CT;
+}
+
+bool Process::operator<=(const Process& second)
+{
+	return CT <= second.CT;
+}
---
+++ Components/Process.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include "../DEFS.h"
+#include "../Basic/Queue.h"
+
+class Process
+{
+private:
+	std::string ID;					// the id of the process
+	ProcessState currentState;
+	CPU_TYPE handlingCPU;
+	int AT;						// arrival time
+	int RT;						// response time
+	int CT;						// CPU time
+	int FCT;					// finished CPU time
+	int TT;						// termination time
+	int TRT;					// turn around time
+	int WT;						// waiting time
+	Queue<Pair<int>> IOList;    // a queue to hold the IO requests of a process
+	Process* myChild;			// this may change to a list or be removed in the future *************
+
+public:
+	Process(std::string id, int inAT, int ct);    // non-defult constructor
+	Process(const Process& origin);				  // copy constructor
+
+	std::string getID() const;					  // getter to the id
+
+	void setProcessState(ProcessState CS);		  // setter to the process state
+	ProcessState getProcessState() const;		  // getter to the process state
+
+	void setHandlingCPU(CPU_TYPE HCPU);			  // sets what type of CPU is handling the process
+	CPU_TYPE getHandlingCPU() const;              // getter to the type of CPU tha is handling the process
+
+	int getAT() const;							  // getter to the arrival time
+
+	void setRT(int rt);							  // setter to the response time
+	int getRT() const;							  // getter to the response time
+
+	int getCT() const;							  // getter to the CPU time
+
+	void updateFCT(int n = 1);					  // increases the finished CPU time by a defult value of 1
+	int getFCT() const;							  // gets the finished CPU time
+
+	void setTT(int tt);							  // sets the termination time and then calculates the TRT and WT
+	int getTT() const;							  // TT getter
+
+	int getTRT() const;							  // total round time getter
+
+	int getWT() const;							  // watting time getter
+
+	Process* getMyChild();						  // gets the child of the Process
+
+	bool operator > (const Process& second);      // compares between two processes in terms of cpu time
+	bool operator < (const Process& second);	  // compares between two processes in terms of cpu time
+	bool operator >= (const Process& second);	  // compares between two processes in terms of cpu time
+	bool operator <= (const Process& second);	  // compares between two processes in terms of cpu time
+};
+
