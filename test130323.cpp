0e53fbe3e49bbc351cdeb32b1808ecd1a0919b5d
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-07 14:14:19 +0200

    testing the priority queue

--- test130323.cpp
+++ test130323.cpp
@@ -1,42 +1,34 @@
 #include "Basic/LinkedList.h" // Note that I ned to specify the path
 // iostream is already included in LinkedList.h 
 #include "Basic/Queue.h"
-#include"Basic/stack.h"
+#include "Basic/stack.h"
+#include "Basic/PriorityQueue.h"
 
 using namespace std;
 int main()
 {
-	LinkedList<int> mylist;
-	for (int i = 0; i < 5; i++)
-	{
-		mylist.insertEnd(6 * i);
-	}
-	mylist.printlist();
+	PriorityQueue<float> p;
+	p.add(4);
+	p.add(3);
+	p.add(2);
+	p.add(5);
+	p.add(3.5);
 
-	Queue<int> myqueue;
-	for (int i = 0; i < 5; i++)
-	{
-		myqueue.push(7 * i);
-	}
-	myqueue.print();
-	/// <summary>
-	/// test stack
-	/// </summary>
-	/// <returns></returns>
-	stack<int> s1(11);
-	stack<float>s2(10);
+	
+	cout << p.peek() << endl;
+	p.show();
+	p.remove();
+	p.remove();
+	p.show();
+	cout << boolalpha << p.isEmpty() << endl;
+	while (p.remove());
+	cout << boolalpha << p.isEmpty() << endl;
+	p.show();
+	p.add(92);
+	p.add(30);
+	p.add(45);
+	p.add(60);
+	p.show();
 
-	stack<float>s4;
-	s1.push(15);
-	s1.push(15);
-	s1.push(15);
-	s1.push(15);
-	s1.push(16);
-	s1.print();
-	cout << endl;
-	stack<int>s3(s1);
-	s3.print();
-	cout << endl;
-	cout << s3.size();
 	return 0;
 }
