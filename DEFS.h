812ebe7b6b7cee9ef011f887ae7bb2b416f753b1
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-07 14:05:24 +0200

    Created the ProcessState enum in the DEFS.h

--- DEFS.h
+++ DEFS.h
@@ -1,1 +1,10 @@
 #pragma once
+
+enum ProcessState
+{
+	NEW,		// in the new list
+	READY,		// in the ready list of a processor
+	RUN,		// is bein run by a processor
+	IO,			// in the block(I/O)
+	TERM 		// is in the terminated list
+};
