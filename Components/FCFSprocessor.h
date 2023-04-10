5d575df24fb48bf9352d24227d6a78fb2783daba
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-10 13:15:01 +0200

    function argument list solving problems

--- Components/FCFSprocessor.h
+++ Components/FCFSprocessor.h
@@ -7,10 +7,10 @@ private:
 	Queue<Process*> RDYlist;
 	
 public:
-	FCFSprocessor(CPU_TYPE type);
+	FCFSprocessor(CPU_TYPE type = FCFS_T);
 	virtual void scheduleAlgo();				//do the operation of the CPU
-	virtual void pullFromRDY(Process*& p) ;
-	virtual void pushToRDY(Process* & p);
+	virtual void pullFromRDY(Process* & p) ;
+	virtual void pushToRDY(Process* p);
 	virtual void updateCPUstate();
 };
 
