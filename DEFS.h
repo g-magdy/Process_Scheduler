eaa9b8b0d531d1891678be6ffc116c09a9c7c49c
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-10 18:17:42 +0200

    Added a new state to the ProcessState enum

--- DEFS.h
+++ DEFS.h
@@ -5,6 +5,7 @@ enum ProcessState
 	NEW,		// in the new list
 	READY,		// in the ready list of a processor
 	RUN,		// is bein run by a processor
+	Blocked,
 	IO,			// in the block(I/O)
 	TERM 		// is in the terminated list
 };
