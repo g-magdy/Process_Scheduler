a42f5e9a4d51bc671e671f28f881e9fa39766ba0
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-11 05:07:05 +0200

    Created the UI class and implemented 3 functions

---
+++ Components/UI.cpp
@@ -0,0 +1,20 @@
+#include "UI.h"
+
+void UI::print(std::string sr)
+{
+	std::cout << sr;
+}
+
+void UI::printConsole()
+{
+	std::cout << "Current Timestep: " << pScheduler->getTimeStep() << std::endl;
+	std::cout << "------------------ RDY processes ------------------" << std::endl;
+	pScheduler->printCPUs(); // here the scheduler should print the "processor " + number of the CPU
+							 // and then make the CPU print it's type and it's ready list
+	std::cout << "------------------ BLK processes ------------------" << std::endl;
+	pScheduler->printBLK();  // this will print the number of processes in the BLK and their IDs
+	std::cout << "------------------ RUN processes ------------------" << std::endl;
+	pScheduler->printRUN();  //it will iterate on every CPU and make it print the process it has in the running state
+	std::cout << "------------------ TRM processes ------------------" << std::endl;
+	pScheduler->printTRM();  // this will print the number of processes terminated and then print them
+}
---
+++ Components/UI.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "../DEFS.h"
+class UI
+{
+private:
+	//Scheduler* pScheduler;        // A Pointer to the Scheduler
+
+public:
+	void print(std::string sr);     // Prints a string given to it with no extra spaces or "endl"
+	RunninMode startUP();			//this will be implemented in phase 2
+	void printConsole();			//this might be int the private if the UI is the one responsble
+									// for running the console
+};
