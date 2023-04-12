dcef3e9862fdc7217039262f594b8dc857c580a0
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-12 22:52:16 +0200

    Overload the print function of the UI to work with pointers

--- Components/UI.h
+++ Components/UI.h
@@ -7,8 +7,17 @@ private:
 	//Scheduler* pScheduler;        // A Pointer to the Scheduler
 
 public:
-	void print(std::string sr);     // Prints a string given to it with no extra spaces or "endl"
 	RunninMode startUP();			//this will be implemented in phase 2
 	void showStatus();			//this might be int the private if the UI is the one responsble
 									// for running the console
+	void print(std::string sr);     // Prints a string given to it with no extra spaces or "endl"
+
+	template<typename T>
+	void print(T* ob);     // Prints a string given to it with no extra spaces or "endl"
 };
+
+template<typename T>
+inline void UI::print(T* ob)
+{
+	std::cout << *ob;
+}
