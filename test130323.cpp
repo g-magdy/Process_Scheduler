25508f85691607702c1118b8296530cd91279fee
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-04-12 21:25:31 +0200

    ADD removeFront & removeEnd for List then check them in the List_test function

--- test130323.cpp
+++ test130323.cpp
@@ -9,38 +9,40 @@ using namespace std;
 
 void List_test(List<string>* listPtr)
 {
-	cout << "Is empty : " << listPtr->isEmpty() << " (should be 1)\n";
-	cout << "Count : " << listPtr->getCount() << " (should be 0)\n\n";
-
-	cout << "testing insertEnd\n";
 	string items[] = { "one", "two", "three", "four", "five", "six" };
 
 	for (int i = 0; i < 6; i++)
 	{
 		listPtr->insertEnd(items[i]);
 	}
-	cout << "Printing list :: ";
-	listPtr->print();
-	cout << "should be one .. .. six\n";
+	
 
-	cout << "Is empty : " << listPtr->isEmpty() << " (should be 0)\n";
-	cout << "Count : " << listPtr->getCount() << " (should be 6)\n\n";
-
-	cout << "clearing list ::\n";
-	listPtr->clear();
-
-	cout << "Is empty : " << listPtr->isEmpty() << " (should be 1)\n";
+	cout << "printing then clearing list using remove front while it is not emty ::\n";
+	while (!listPtr->isEmpty())
+	{
+		cout << listPtr->front() << " ";
+		listPtr->removeFront();
+	}
+	cout << "\nIs empty : " << listPtr->isEmpty() << " (should be 1)\n";
 	cout << "Count : " << listPtr->getCount() << " (should be 0)\n\n";
 
-	cout << "testing begin and end\n";
+	cout << "New data ..\n";
 	for (int i = 0; i < 3; i++)
 	{
 		listPtr->insertBegin(items[i]);
 		listPtr->insertEnd(items[i]);
 	}
-	cout << "Printing list :: ";
-	listPtr->print();
-	cout << "should be 3 2 1 1 2 3 (in letters)\n";	
+	cout << "Is empty : " << listPtr->isEmpty() << " (should be 0)\n";
+	cout << "Count : " << listPtr->getCount() << " (should be 6)\n\n";
+	
+
+	cout << "clearing list using remove last while it is not emty ::\n";
+	while (!listPtr->isEmpty())
+	{
+		listPtr->removeLast();
+	}
+	cout << "Is empty : " << listPtr->isEmpty() << " (should be 1)\n";
+	cout << "Count : " << listPtr->getCount() << " (should be 0)\n\n";
 }
 
 int main()
