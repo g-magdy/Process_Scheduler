b5866f372edacfb657e4c7f83375953c728b13e9
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-16 11:22:57 +0200

    check the logic of the RR CPUs

--- Components/RRprocessor.cpp
+++ Components/RRprocessor.cpp
@@ -6,30 +6,32 @@ RRprocessor::RRprocessor(Scheduler* pscheduler) :Processor(pscheduler, RR_T)
 
 void RRprocessor::scheduleAlgo(int currentTimeStep)
 {
-	Process* ptr = getRunnignProcess();
+	Process* ptr = nullptr;
 
-	if (getCPUstate() == IDLE)						//related to phase one 
+	if (getCPUstate() == IDLE && !RDY.isEmpty())		//related to phase one 
+														//if there is no a running process update it 
 	{
-		
-		if (!RDY.empty())
-		{
-			pullFromRDY(ptr);							//get new process from ready list
+		pullFromRDY(ptr);								//get new process from ready list
 
-			setRunnignProcess(ptr);						//set this process as runing process
+		setRunningProcess(ptr);							//set this process as runing process
 
-			ptr->setProcessState(RUN);					//change the state of the new added process
+		ptr->setProcessState(RUN);						//change the state of the new added process
 
-			ptr->setResponseT(currentTimeStep);
-		}
-
+		ptr->setResponseT(currentTimeStep);
 	}
+
+	ptr = getRunningProcess();
+
 	if (ptr)
 	{
 		updateTotalCpuTime();							// increament total CPU time of this processor by the cpu time of this process
-		ptr->updateFinishedCPUT();						//increament the CPU time of this process}
+		ptr->updateFinishedCPUT();						//increament the CPU time of this process
+
+		//we need to handle if the Process needs IO     ******************************************************************************
 	}
-	else
-		return;											//if there is no running process
+
+	updateCPUstate();											//if there is no running process
+	
 	double randNum = rand() * 100;
 
 	if (randNum >= 1 && randNum <= 15)
@@ -40,10 +42,7 @@ void RRprocessor::scheduleAlgo(int currentTimeStep)
 		//pScheduler->moveToTRM;					//move to termination queue
 	if (randNum >= 10)
 		//pScheduler->movefromBLKtoRDY;				//move from IO queue to ready
-
-	updateCPUstate();								//update CPU state 
-	//need to modifiy
-
+	;
 }
 
 
--- test130323.cpp
+++ test130323.cpp
@@ -7,24 +7,53 @@
 
 using namespace std;
 
-void List_test(List<char>* listPtr)
+void List_test(List<Process*>& mylist)
 {
-	char items[] = { 'a', 'b', 'c', 'd', 'e', 'f'};
+	Process* p = new Process("83", 8, 12);
+	Process* n = new Process("34", 8, 12);
+	Process* r = new Process("21", 8, 12);
+	Process* s = new Process("93", 8, 12);
+	Process* m = new Process("143", 8, 12);
 
-	for (int i = 0; i < 6; i++)
-	{
-		listPtr->push_back(items[i]);
-	}
-	cout << "Found a ? (should be 1)" << listPtr->find('a') << endl;
-	cout << "Found c ? (should be 1)" << listPtr->find('c') << endl;
-	cout << "Found f ? (should be 1)" << listPtr->find('f') << endl;
-	listPtr->pop_back();
-	listPtr->pop_front();
-	cout << " after removing front and back :: \n";
-	cout << "Found one ? (should be 0)" << listPtr->find('a') << endl;
-	cout << "Found six ? (should be 0)" << listPtr->find('f') << endl;
+	mylist.push_front(n);
+	mylist.push_front(p);
+	mylist.push_back(r);
+	mylist.push_back(s);
+	mylist.push_back(m);
+	mylist.print(); // 83,34, 21, 93, 143
+	cout << endl;
+	cout << boolalpha << mylist.find("24") << endl; //false
+	cout << boolalpha << mylist.find("93") << endl; //true
+	mylist.pop_back();
+	mylist.pop_front();
+	mylist.print(); //34, 21, 93
+	Process* temp;
+	mylist.pop_front(temp);
+	cout << endl;
+	cout << temp->getID() << endl; // 34
+	cout << mylist.getCount() << endl; //2
+	mylist.print(); //21, 93
 }
+void test_priorityQ(PriorityQueue<Process*> pq)
+{
+	cout << "\nTesting the PriorityQueue.\n";
+	Process* p = new Process("83", 8, 12);
+	Process* n = new Process("34", 8, 5);
+	Process* r = new Process("21", 8, 1);
+	Process* s = new Process("93", 8, 46);
+	Process* m = new Process("143", 8, 34);
 
+	pq.push(p);
+	pq.push(n);
+	pq.push(r);
+	pq.push(s);
+	pq.push(m);
+	pq.print();
+	Process* poped;
+	pq.pop(poped);
+	cout << endl << poped->getID() << endl;
+	pq.print();
+}
 void removeTest1(List<string>* listPtr)
 {
 	cout << "\n//////////////////\n";
@@ -63,9 +92,13 @@ void removeTest2(List<Process*>* listPtr)
 	listPtr->print();
 
 	cout << "\n\nafter removing 83, 21, 143 : \n";
-	listPtr->removeByID(p->getID());
-	listPtr->removeByID(r->getID());
-	listPtr->removeByID(m->getID());
+	Process* del = nullptr;
+	del = listPtr->removeByID(p->getID());
+	cout << del->getID() << "removed\n";
+	del = listPtr->removeByID(r->getID());
+	cout << del->getID() << "removed\n";
+	del = listPtr->removeByID(m->getID());
+	cout << del->getID() << "removed\n";
 	cout << "count :" << listPtr->getCount()<<'\n';
 	listPtr->print();
 	cout << "\n//////////////////\n";
@@ -79,35 +112,17 @@ void print(T* ob)
 
 int main()
 {
-	Process* p = new Process("83", 8, 12);
-	Process* n = new Process("34", 8, 12);
-	Process* r = new Process("21", 8, 12);
-	Process* s = new Process("93", 8, 12);
-	Process* m = new Process("143", 8, 12);
 	List<Process*>mylist;
-
-	mylist.push_front(n);
-	mylist.push_front(p);
-	mylist.push_back(r);
-	mylist.push_back(s);
-	mylist.push_back(m);
-	mylist.print(); // 83,34, 21, 93, 143
-	cout << endl;
-	cout << boolalpha << mylist.find("24") << endl; //false
-	cout << boolalpha << mylist.find("93") << endl; //true
-	mylist.pop_back();
-	mylist.pop_front();
-	mylist.print(); //34, 21, 93
-	Process* temp;
-	mylist.pop_front(temp);
-	cout << endl;
-	cout << temp->getID() << endl; // 34
-	cout << mylist.getCount() << endl; //2
-	mylist.print(); //21, 93
+	List_test(mylist); //testing all the functions of the list
 
 	List<string>* listPtr1 = new List<string>;
+	removeTest1(listPtr1); //tests remove by index
+
 	List<Process*>* listPtr2 = new List<Process*>;
-	//removeTest1(listPtr);
-	removeTest2(listPtr2);
+	//removeTest2(listPtr2); //tests remove by ID in the list ADT
+	
+	PriorityQueue<Process*> list3;
+	test_priorityQ(list3);
+
 	return 0;
 }
