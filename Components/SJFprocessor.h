d4ce8a6db32479ee4b7c3f23d5306b0d52a06d6d
Ahmed Aladdin <racerahmed40@gmail.com>
2023-04-17 22:11:04 +0200

    same as last one

--- Components/SJFprocessor.h
+++ Components/SJFprocessor.h
@@ -11,6 +11,7 @@ public:
 	virtual void scheduleAlgo(int currentTimeStep);			
 	virtual void pullFromRDY(Process*& p);
 	virtual void pushToRDY(Process* p);
+	virtual void printRDYList();
 	virtual bool kill(std::string idtoKill) { return false; }
 	virtual bool fork() { return false; }
 };
